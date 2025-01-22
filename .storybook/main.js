6306febc54b80755af8c7af9bdcdd45b553b934a
dev-lan <devinscottlange@gmail.com>
2025-01-22 14:39:49 -0700

    limit stories to components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,18 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/components/**/*.mdx',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/vue3-vite',
-    options: {}
-  }
-}
-export default config
+    options: {},
+  },
+};
+export default config;
