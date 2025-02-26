4b50714b45d6f542e14475a9bdb1e03e3b46d4db
dev-lan <devinscottlange@gmail.com>
2025-02-26 14:58:26 -0700

    improve formatting of examples page

--- src/pages/ExamplesPage.vue
+++ src/pages/ExamplesPage.vue
@@ -19,8 +19,148 @@ const exampleGroups: ExampleGroup[] = [
     name: 'Bar Charts',
     examples: [
       {
-        name: 'Simple Bar Chart',
-        thumbnail: 'https://via.placeholder.com/50',
+        name: 'Simple Bar Chart! 2asdf asdf asdf asdf asdf ',
+        thumbnail: './example_thumbnails/bar_charts/simple_bar_chart.png',
+        spec: {
+          source: {
+            name: 'donors',
+            source: './data/donors.csv',
+          },
+          transformation: [
+            {
+              groupby: 'sex',
+            },
+            {
+              rollup: {
+                mean_mass: { op: 'mean', field: 'weight_value' },
+              },
+            },
+          ],
+          representation: {
+            mark: 'bar',
+            mapping: [
+              { encoding: 'x', field: 'sex', type: 'nominal' },
+              { encoding: 'y', field: 'mean_mass', type: 'quantitative' },
+            ],
+          },
+        },
+      },
+      {
+        name: 'Simple Bar Chart222222222 azlsdfj alsdkf ',
+        thumbnail: './example_thumbnails/test/image.png',
+        spec: {
+          source: {
+            name: 'donors',
+            source: './data/donors.csv',
+          },
+          transformation: [
+            {
+              groupby: 'sex',
+            },
+            {
+              rollup: {
+                mean_mass: { op: 'mean', field: 'weight_value' },
+              },
+            },
+          ],
+          representation: {
+            mark: 'bar',
+            mapping: [
+              { encoding: 'x', field: 'sex', type: 'nominal' },
+              { encoding: 'y', field: 'mean_mass', type: 'quantitative' },
+            ],
+          },
+        },
+      },
+      {
+        name: 'Simple Bar Chart3',
+        thumbnail: './example_thumbnails/bar_charts/simple_bar_chart.png',
+        spec: {
+          source: {
+            name: 'donors',
+            source: './data/donors.csv',
+          },
+          transformation: [
+            {
+              groupby: 'sex',
+            },
+            {
+              rollup: {
+                mean_mass: { op: 'mean', field: 'weight_value' },
+              },
+            },
+          ],
+          representation: {
+            mark: 'bar',
+            mapping: [
+              { encoding: 'x', field: 'sex', type: 'nominal' },
+              { encoding: 'y', field: 'mean_mass', type: 'quantitative' },
+            ],
+          },
+        },
+      },
+    ],
+  },
+  {
+    name: 'Bar Charts 2',
+    examples: [
+      {
+        name: 'Simple Bar Chart 4',
+        thumbnail: './example_thumbnails/bar_charts/simple_bar_chart.png',
+        spec: {
+          source: {
+            name: 'donors',
+            source: './data/donors.csv',
+          },
+          transformation: [
+            {
+              groupby: 'sex',
+            },
+            {
+              rollup: {
+                mean_mass: { op: 'mean', field: 'weight_value' },
+              },
+            },
+          ],
+          representation: {
+            mark: 'bar',
+            mapping: [
+              { encoding: 'x', field: 'sex', type: 'nominal' },
+              { encoding: 'y', field: 'mean_mass', type: 'quantitative' },
+            ],
+          },
+        },
+      },
+      {
+        name: 'Simple Bar Chart 5',
+        thumbnail: './example_thumbnails/bar_charts/simple_bar_chart.png',
+        spec: {
+          source: {
+            name: 'donors',
+            source: './data/donors.csv',
+          },
+          transformation: [
+            {
+              groupby: 'sex',
+            },
+            {
+              rollup: {
+                mean_mass: { op: 'mean', field: 'weight_value' },
+              },
+            },
+          ],
+          representation: {
+            mark: 'bar',
+            mapping: [
+              { encoding: 'x', field: 'sex', type: 'nominal' },
+              { encoding: 'y', field: 'mean_mass', type: 'quantitative' },
+            ],
+          },
+        },
+      },
+      {
+        name: 'Simple Bar Chart 6',
+        thumbnail: './example_thumbnails/bar_charts/simple_bar_chart.png',
         spec: {
           source: {
             name: 'donors',
@@ -52,25 +192,53 @@ const exampleGroups: ExampleGroup[] = [
 function getUrlWithSpec(spec: UDIGrammar): string {
   const stringified = JSON.stringify(spec, null, 2);
   const compressed = compressToEncodedURIComponent(stringified);
-  return `./#/Editor?spec=${compressed}`;
+  return `/Editor?spec=${compressed}`;
 }
 </script>
 <template>
-  <q-page class="row items-center justify-evenly">
-    <template v-for="group in exampleGroups" :key="group.name">
-      <div>
-        <h3>{{ group.name }}</h3>
-        <ul>
-          <li
-            v-for="example in group.examples"
-            :key="example.name"
-            style="cursor: pointer"
+  <q-page class="column items-center justify-start q-ma-md">
+    <div
+      v-for="group in exampleGroups"
+      :key="group.name"
+      class="column full-width"
+    >
+      <div class="text-h6 text-primary">{{ group.name }}</div>
+      <q-separator class="full-width" />
+      <div class="row q-mb-md q-mt-sm">
+        <q-card
+          v-for="example in group.examples"
+          :key="example.name"
+          class="q-pa-sm q-ma-sm flex column items-center"
+        >
+          <!-- <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+            <div class="absolute-bottom text-subtitle2 text-center">Title</div>
+          </q-img> -->
+
+          <q-img
+            ratio="1"
+            fit="scale-down"
+            :src="example.thumbnail"
+            class="square"
           >
-            <a :href="getUrlWithSpec(example.spec)">{{ example.name }}</a>
-            <img :src="example.thumbnail" style="width: 50px" />
-          </li>
-        </ul>
+          </q-img>
+          <q-card-actions vertical>
+            <q-btn
+              color="primary"
+              rounded
+              no-caps
+              icon-right="open_in_new"
+              :to="getUrlWithSpec(example.spec)"
+              :label="example.name"
+            ></q-btn>
+          </q-card-actions>
+        </q-card>
       </div>
-    </template>
+    </div>
   </q-page>
 </template>
+<style lang="scss">
+.square {
+  height: 200px;
+  width: 200px;
+}
+</style>
