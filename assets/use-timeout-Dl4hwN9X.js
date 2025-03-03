8546c79ccb3ca31ca45bd38a3d3e0f6c35417a3e
Dev-Lan <Dev-Lan@users.noreply.github.com>
2025-03-03 17:23:53 +0000

    deploy: db72e301a10d42f749ab5088911853d74b780708

---
+++ assets/use-timeout-Dl4hwN9X.js
@@ -0,0 +1,1 @@
+import{v as u}from"./QSpinner-Vo5ctVb0.js";import{y as i,f as m,g as s}from"./index-GNPbUQmn.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
