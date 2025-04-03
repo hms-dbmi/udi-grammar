3b38b72a3d968897a561348841f3c82d70109afc
dev-lan <devinscottlange@gmail.com>
2025-04-03 16:36:27 -0600

    activate pinia and publish 0.0.2 on npm

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+import { createPinia, getActivePinia } from 'pinia';
 import UDIVis from './UDIVis.vue';
 import TableComponent from './TableComponent.vue';
 import VegaLite from './VegaLite.vue';
@@ -5,6 +6,12 @@ import VegaLite from './VegaLite.vue';
 export { UDIVis, TableComponent, VegaLite };
 
 export function install(app: any) {
+  // Ensure Pinia is initialized before adding components
+  if (!getActivePinia()) {
+    const pinia = createPinia();
+    app.use(pinia);
+  }
+
   app.component('UDIVis', UDIVis);
   app.component('TableComponent', TableComponent);
   app.component('VegaLite', VegaLite);
