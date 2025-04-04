df11cc2f7dfeda84b6154a3ec9fcc0dfa31901d6
dev-lan <devinscottlange@gmail.com>
2025-04-03 18:09:53 -0600

    export UDIToolkit with instll function

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,16 +3,17 @@ import UDIVis from './UDIVis.vue';
 import TableComponent from './TableComponent.vue';
 import VegaLite from './VegaLite.vue';
 
-export { UDIVis, TableComponent, VegaLite };
+const UDIToolkit = {
+  install(app: any) {
+    // Ensure Pinia is initialized before adding components
+    if (!getActivePinia()) {
+      const pinia = createPinia();
+      app.use(pinia);
+    }
 
-export function install(app: any) {
-  // Ensure Pinia is initialized before adding components
-  if (!getActivePinia()) {
-    const pinia = createPinia();
-    app.use(pinia);
-  }
-
-  app.component('UDIVis', UDIVis);
-  app.component('TableComponent', TableComponent);
-  app.component('VegaLite', VegaLite);
-}
+    app.component('UDIVis', UDIVis);
+    app.component('TableComponent', TableComponent);
+    app.component('VegaLite', VegaLite);
+  },
+};
+export { UDIToolkit, UDIVis, TableComponent, VegaLite };
