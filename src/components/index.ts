3347bacf2b3eab5a029aa551549bbc3666066a32
dev-lan <devinscottlange@gmail.com>
2025-05-09 18:59:57 -0600

    new version, include cell renderer in package

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import { createPinia, getActivePinia } from 'pinia';
 import UDIVis from './UDIVis.vue';
 import TableComponent from './TableComponent.vue';
 import VegaLite from './VegaLite.vue';
+import UDICellRenderer from './UDICellRenderer.vue';
 
 const UDIToolkit = {
   install(app: any) {
@@ -12,8 +13,9 @@ const UDIToolkit = {
     }
 
     app.component('UDIVis', UDIVis);
-    app.component('TableComponent', TableComponent);
     app.component('VegaLite', VegaLite);
+    app.component('TableComponent', TableComponent);
+    app.component('UDICellRenderer', UDICellRenderer);
   },
 };
 export { UDIToolkit, UDIVis, TableComponent, VegaLite };
