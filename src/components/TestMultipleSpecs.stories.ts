ff653cb21786d4c3a4de4e4ff2c16a02666e2b3d
dev-lan <devinscottlange@gmail.com>
2025-07-17 14:02:24 -0600

    remove wip story

--- src/components/TestMultipleSpecs.stories.ts
+++ src/components/TestMultipleSpecs.stories.ts
@@ -1096,57 +1096,57 @@ export const CrossFilterStripPlot = {
   },
 };
 
-export const PointSelection = {
-  args: {
-    specs: [
-      {
-        source: {
-          name: 'donors',
-          source: './data/donors.csv',
-        },
-        transformation: [
-          {
-            filter: {
-              name: 'scatter-select',
-            },
-          },
-          {
-            groupby: 'sex',
-          },
-          {
-            rollup: {
-              sex_count: { op: 'count' },
-            },
-          },
-        ],
-        representation: {
-          mark: 'bar',
-          mapping: [
-            { encoding: 'x', field: 'sex', type: 'nominal' },
-            { encoding: 'y', field: 'sex_count', type: 'quantitative' },
-          ],
-        },
-      },
-      {
-        source: {
-          name: 'donors',
-          source: './data/donors.csv',
-        },
-        representation: {
-          mark: 'point',
-          mapping: [
-            { encoding: 'y', field: 'height_value', type: 'quantitative' },
-            { encoding: 'x', field: 'weight_value', type: 'quantitative' },
-          ],
-          select: {
-            name: 'scatter-select',
-            how: {
-              type: 'interval',
-              on: 'xy',
-            },
-          },
-        },
-      },
-    ],
-  },
-};
+// export const PointSelection = {
+//   args: {
+//     specs: [
+//       {
+//         source: {
+//           name: 'donors',
+//           source: './data/donors.csv',
+//         },
+//         transformation: [
+//           {
+//             filter: {
+//               name: 'scatter-select',
+//             },
+//           },
+//           {
+//             groupby: 'sex',
+//           },
+//           {
+//             rollup: {
+//               sex_count: { op: 'count' },
+//             },
+//           },
+//         ],
+//         representation: {
+//           mark: 'bar',
+//           mapping: [
+//             { encoding: 'x', field: 'sex', type: 'nominal' },
+//             { encoding: 'y', field: 'sex_count', type: 'quantitative' },
+//           ],
+//         },
+//       },
+//       {
+//         source: {
+//           name: 'donors',
+//           source: './data/donors.csv',
+//         },
+//         representation: {
+//           mark: 'point',
+//           mapping: [
+//             { encoding: 'y', field: 'height_value', type: 'quantitative' },
+//             { encoding: 'x', field: 'weight_value', type: 'quantitative' },
+//           ],
+//           select: {
+//             name: 'scatter-select',
+//             how: {
+//               type: 'interval',
+//               on: 'xy',
+//             },
+//           },
+//         },
+//       },
+//     ],
+//   },
+// };
