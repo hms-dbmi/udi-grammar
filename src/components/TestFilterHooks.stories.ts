f8ca575a512758124de1c5355ba50b4dc6bed039
dev-lan <devinscottlange@gmail.com>
2025-08-16 18:56:20 -0600

    add xy tests

--- src/components/TestFilterHooks.stories.ts
+++ src/components/TestFilterHooks.stories.ts
@@ -17,14 +17,16 @@ export default {
   // },
 };
 
-export const ReadFilterState = {
+export const ReadFilterStateX = {
   args: {
     testType: 'read',
-    selections: {
-      selectionName: 'weight-select',
-      entity: 'donors',
-      field: 'weight_value',
-    },
+    selections: [
+      {
+        selectionName: 'weight-select',
+        entity: 'donors',
+        field: 'weight_value',
+      },
+    ],
     spec: {
       source: {
         name: 'donors',
@@ -33,7 +35,7 @@ export const ReadFilterState = {
       transformation: [
         {
           filter: {
-            name: 'scatter-select',
+            name: 'weight-select',
           },
         },
       ],
@@ -54,17 +56,63 @@ export const ReadFilterState = {
     },
   },
 };
+export const ReadFilterStateXY = {
+  args: {
+    testType: 'read',
+    selections: [
+      {
+        selectionName: 'height-weight-select',
+        entity: 'donors',
+        field: 'weight_value',
+      },
+      {
+        selectionName: 'height-weight-select',
+        entity: 'donors',
+        field: 'height_value',
+      },
+    ],
+    spec: {
+      source: {
+        name: 'donors',
+        source: './data/donors.csv',
+      },
+      transformation: [
+        {
+          filter: {
+            name: 'height-weight-select',
+          },
+        },
+      ],
+      representation: {
+        mark: 'point',
+        mapping: [
+          { encoding: 'y', field: 'height_value', type: 'quantitative' },
+          { encoding: 'x', field: 'weight_value', type: 'quantitative' },
+        ],
+        select: {
+          name: 'height-weight-select',
+          how: {
+            type: 'interval',
+            on: 'xy',
+          },
+        },
+      },
+    },
+  },
+};
 
-export const WriteFilterState = {
+export const WriteFilterStateX = {
   args: {
     testType: 'write',
-    selections: {
-      selectionName: 'weight-select',
-      entity: 'donors',
-      field: 'weight_value',
-      minValue: 0,
-      maxValue: 160,
-    },
+    selections: [
+      {
+        selectionName: 'weight-select',
+        entity: 'donors',
+        field: 'weight_value',
+        minValue: 0,
+        maxValue: 160,
+      },
+    ],
     spec: {
       source: {
         name: 'donors',
@@ -95,16 +143,65 @@ export const WriteFilterState = {
   },
 };
 
-export const ReadWriteLinkedFilterState = {
+export const WriteFilterStateXY = {
+  args: {
+    testType: 'write',
+    selections: [
+      {
+        selectionName: 'weight-select',
+        entity: 'donors',
+        field: 'weight_value',
+        minValue: 0,
+        maxValue: 160,
+      },
+      {
+        selectionName: 'height-select',
+        entity: 'donors',
+        field: 'height_value',
+        minValue: 50,
+        maxValue: 200,
+      },
+    ],
+    spec: {
+      source: {
+        name: 'donors',
+        source: './data/donors.csv',
+      },
+      transformation: [
+        {
+          filter: {
+            name: 'weight-select',
+          },
+        },
+        {
+          filter: {
+            name: 'height-select',
+          },
+        },
+      ],
+      representation: {
+        mark: 'point',
+        mapping: [
+          { encoding: 'y', field: 'height_value', type: 'quantitative' },
+          { encoding: 'x', field: 'weight_value', type: 'quantitative' },
+        ],
+      },
+    },
+  },
+};
+
+export const ReadWriteFilterStateX = {
   args: {
     testType: 'linked',
-    selections: {
-      selectionName: 'weight-select',
-      entity: 'donors',
-      field: 'weight_value',
-      minValue: 0,
-      maxValue: 160,
-    },
+    selections: [
+      {
+        selectionName: 'weight-select',
+        entity: 'donors',
+        field: 'weight_value',
+        minValue: 0,
+        maxValue: 160,
+      },
+    ],
     spec: {
       source: {
         name: 'donors',
@@ -135,16 +232,18 @@ export const ReadWriteLinkedFilterState = {
   },
 };
 
-export const ReadWriteLinkedFilterStateY = {
+export const ReadWriteFilterStateY = {
   args: {
     testType: 'linked',
-    selections: {
-      selectionName: 'height-select',
-      entity: 'donors',
-      field: 'height_value',
-      minValue: 50,
-      maxValue: 200,
-    },
+    selections: [
+      {
+        selectionName: 'height-select',
+        entity: 'donors',
+        field: 'height_value',
+        minValue: 50,
+        maxValue: 200,
+      },
+    ],
     spec: {
       source: {
         name: 'donors',
@@ -168,6 +267,54 @@ export const ReadWriteLinkedFilterStateY = {
           how: {
             type: 'interval',
             on: 'y',
+          },
+        },
+      },
+    },
+  },
+};
+export const ReadWriteFilterStateXY = {
+  args: {
+    testType: 'linked',
+    selections: [
+      {
+        selectionName: 'height-weight-select',
+        entity: 'donors',
+        field: 'height_value',
+        minValue: 50,
+        maxValue: 200,
+      },
+      {
+        selectionName: 'height-weight-select',
+        entity: 'donors',
+        field: 'weight_value',
+        minValue: 0,
+        maxValue: 160,
+      },
+    ],
+    spec: {
+      source: {
+        name: 'donors',
+        source: './data/donors.csv',
+      },
+      transformation: [
+        {
+          filter: {
+            name: 'height-weight-select',
+          },
+        },
+      ],
+      representation: {
+        mark: 'point',
+        mapping: [
+          { encoding: 'y', field: 'height_value', type: 'quantitative' },
+          { encoding: 'x', field: 'weight_value', type: 'quantitative' },
+        ],
+        select: {
+          name: 'height-weight-select',
+          how: {
+            type: 'interval',
+            on: 'xy',
           },
         },
       },
--- src/components/TestFilterHooks.vue
+++ src/components/TestFilterHooks.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { ref, computed, watch, onMounted } from 'vue';
 import type { ParsedUDIGrammar } from './Parser';
-import type { DataSelection, DataSelections } from './DataSourcesStore';
+import type { DataSelections } from './DataSourcesStore';
 
 interface TestFilterHooksProps {
   spec: ParsedUDIGrammar;
-  selections?: SelectionParams;
+  selections?: SelectionParams[];
   testType: 'read' | 'write' | 'linked';
 }
 
@@ -19,36 +19,63 @@ interface SelectionParams {
 
 const props = defineProps<TestFilterHooksProps>();
 
-const rangeModel = ref({
-  min: props?.selections?.minValue,
-  max: props?.selections?.maxValue,
-});
+// const rangeModel = ref({
+//   min: props?.selections?.minValue,
+//   max: props?.selections?.maxValue,
+// });
 
-const selections = computed(() => {
-  // console.log('filter hook selections recalculated');
-  return {
-    [props?.selections?.selectionName ?? 'UNKNOWN_SELECTION_NAME']: {
-      type: 'interval',
-      dataSourceKey: props?.selections?.entity,
-      selection: {
-        [props?.selections?.field ?? 'UNKONWN_FIELD']: [
-          rangeModel.value.min,
-          rangeModel.value.max,
-        ],
-      },
-    },
-  };
+const rangeModels = ref<
+  { min: number; max: number; selectionName: string; field: string }[]
+>([]);
+for (const selection of props.selections || []) {
+  rangeModels.value.push({
+    min: selection.minValue ?? 0,
+    max: selection.maxValue ?? 100,
+    selectionName: selection.selectionName,
+    field: selection.field,
+  });
+}
+
+const udiVisSelections = computed(() => {
+  const visSelections = {};
+  if (!props.selections) return visSelections;
+  for (let i = 0; i < props.selections.length; i++) {
+    const selection = props.selections[i];
+    const rangeModel = rangeModels.value[i];
+    if (!selection || !rangeModel) continue;
+    if (!(selection.selectionName in visSelections)) {
+      visSelections[selection.selectionName] = {
+        type: 'interval',
+        dataSourceKey: selection.entity,
+        selection: {
+          [selection.field]: [rangeModel.min, rangeModel.max],
+        },
+      };
+    } else {
+      visSelections[selection.selectionName].selection[selection.field] = [
+        rangeModel.min,
+        rangeModel.max,
+      ];
+    }
+  }
+  return visSelections;
 });
 
 function handleSelectionChange(selection: DataSelections) {
-  // console.log('handle selection change');
-  const updatedRange =
-    selection[props?.selections?.selectionName ?? 'UNKNOWN_SELECTION_NAME']
-      ?.selection?.[props?.selections?.field ?? 'UNKNOWN_FIELD'];
-  if (!updatedRange) return;
-  if (updatedRange) {
-    rangeModel.value.min = updatedRange[0];
-    rangeModel.value.max = updatedRange[1];
+  // console.log('handle selection change: ', selection);
+  for (const rangeModel of rangeModels.value) {
+    const selectionName = rangeModel.selectionName;
+    if (selection[selectionName]) {
+      const updatedRange =
+        selection[selectionName].selection[
+          props.selections?.find((s) => s.field === rangeModel.field)?.field ??
+            'UNKNOWN_FIELD'
+        ];
+      if (updatedRange) {
+        rangeModel.min = updatedRange[0];
+        rangeModel.max = updatedRange[1];
+      }
+    }
   }
 }
 </script>
@@ -56,56 +83,67 @@ function handleSelectionChange(selection: DataSelections) {
 <template>
   <template v-if="props.testType === 'read'">
     <h2>Read Test</h2>
-    <div>Min: {{ rangeModel.min }}</div>
-    <div>Max: {{ rangeModel.max }}</div>
+    <template v-for="(rangeModel, index) in rangeModels" :key="index">
+      <div>{{ props.selections[index].field }}</div>
+      <div>Min: {{ rangeModel.min }}</div>
+      <div>Max: {{ rangeModel.max }}</div>
+      <hr />
+    </template>
     <UDIVis :spec="spec" @selection-change="handleSelectionChange"> </UDIVis>
   </template>
   <template v-if="props.testType === 'write'">
-    <h2>Read Test</h2>
-    <div>
-      <input
-        type="range"
-        v-model="rangeModel.min"
-        :min="props.selections.minValue"
-        :max="props.selections.maxValue"
-      />
-      Min: {{ rangeModel.min }}
-    </div>
-    <div>
-      <input
-        type="range"
-        v-model="rangeModel.max"
-        :min="props.selections.minValue"
-        :max="props.selections.maxValue"
-      />
-      Max: {{ rangeModel.max }}
-    </div>
-    <UDIVis :spec="spec" :selections="selections"> </UDIVis>
+    <h2>Write Test</h2>
+    <template v-for="(rangeModel, index) in rangeModels" :key="index">
+      <div>{{ props.selections[index].field }}</div>
+      <div>
+        <input
+          type="range"
+          v-model="rangeModel.min"
+          :min="props.selections[index].minValue"
+          :max="props.selections[index].maxValue"
+        />
+        Min: {{ rangeModel.min }}
+      </div>
+      <div>
+        <input
+          type="range"
+          v-model="rangeModel.max"
+          :min="props.selections[index].minValue"
+          :max="props.selections[index].maxValue"
+        />
+        Max: {{ rangeModel.max }}
+      </div>
+      <hr />
+    </template>
+    <UDIVis :spec="spec" :selections="udiVisSelections"></UDIVis>
   </template>
 
   <template v-if="props.testType === 'linked'">
-    <h2>Read Test</h2>
-    <div>
-      <input
-        type="range"
-        v-model.number="rangeModel.min"
-        :min="props.selections.minValue"
-        :max="props.selections.maxValue"
-      />
-      Min: {{ rangeModel.min }}
-    </div>
-    <div>
-      <input
-        type="range"
-        v-model.number="rangeModel.max"
-        :min="props.selections.minValue"
-        :max="props.selections.maxValue"
-      />
-      Max: {{ rangeModel.max }}
-    </div>
+    <h2>Linked Test</h2>
+    <template v-for="(rangeModel, index) in rangeModels" :key="index">
+      <div>{{ props.selections[index].field }}</div>
+      <div>
+        <input
+          type="range"
+          v-model.number="rangeModel.min"
+          :min="props.selections[index].minValue"
+          :max="props.selections[index].maxValue"
+        />
+        Min: {{ rangeModel.min }}
+      </div>
+      <div>
+        <input
+          type="range"
+          v-model.number="rangeModel.max"
+          :min="props.selections[index].minValue"
+          :max="props.selections[index].maxValue"
+        />
+        Max: {{ rangeModel.max }}
+      </div>
+    </template>
     <UDIVis
       :spec="spec"
-      :selections="selections"
+      :selections="udiVisSelections"
       @selection-change="handleSelectionChange"
     >
     </UDIVis>
